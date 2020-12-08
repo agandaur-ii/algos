12ad556e276fc97310ff276e833ab4d68b754428
Agandaur <cjwagstaffe@gmail.com>
2020-12-08 00:45:37 -0800

    number of lines attempt

--- Try_again/numberOfLines.js
+++ Try_again/numberOfLines.js
@@ -32,3 +32,19 @@
 // widths is an array of length 26.
 // widths[i] will be in the range of [2, 10].
 
+// found solution:
+
+var numberOfLines = function(widths, S) {
+    let lines = 1;
+    let last = 0;
+    const base = 'a'.charCodeAt(0);
+    for (let i = 0; i < S.length; i++) {
+      const width = widths[S.charCodeAt(i) - base];
+      last += width;
+      if (last > 100) {
+        lines++;
+        last = width;
+      }
+    }
+    return [lines, last];
+};
